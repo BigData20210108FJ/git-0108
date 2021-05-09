f5c60acc1f6096e25990d25d26c038f5e9b741ac
FangJun <1440716468@qq.com>
2021-05-09 22:35:13 +0800

    master commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hot_fix");
+        System.out.println("master Test");
     }
 }
